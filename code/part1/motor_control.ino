917608c0f89ef8aeaedc15b7a28de75cd2d4c07f
sahil <sahil11panindre@gmail.com>
2021-02-25 22:58:30 +0530

    Add files via upload

---
+++ code/part1/motor_control.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // 
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW); 
+  digitalWrite(8, LOW);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
